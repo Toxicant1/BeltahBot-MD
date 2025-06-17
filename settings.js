150b50a18e0fcaf518b1230e3f10e78f20f7aa83
Toxicant1 <isaac0maina@gmail.com>
2025-06-17 22:24:26 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,13 +1,13 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
-  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
-  version: "2.0.3",
+    packname: 'BeltaBot',
+    author: '',
+    botName: 'BeltaBot',
+    botOwner: 'Ishaq Ibrahim', // Your name
+    ownerNumber: '254741819582', // Your number in international format
+    giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+    commandMode: 'public',
+    description: 'This is a bot for managing group commands.',
+    version: '2.0.3',
 };
 
 module.exports = settings;
